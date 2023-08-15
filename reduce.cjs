7d94af5bbedac65e92b339917d8926674f6b4040
BushraF6 <f17.bush21@gmail.com>
2023-08-15 21:38:52 +0530

    created reduce function

---
+++ reduce.cjs
@@ -0,0 +1,12 @@
+function doReduce (arr, func, initial) {
+    let result = initial;
+
+    for (let i = 0; i < arr.length; i++) {
+
+        result = func(result, arr[i])
+    }
+
+    return result;
+}
+
+module.exports = doReduce;
